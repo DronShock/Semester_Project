ce117d2bd57e445915ddccb089d09b0cb6bacc02
khomiakov <khomiakov.io@phystech.edu>
2020-11-18 19:17:24 +0300

    base

---
+++ body.py
@@ -0,0 +1,47 @@
+import pygame as p
+
+
+class obj():
+    class creature():
+        def __init__(self):
+            self.x = x
+            self.y = y
+            self.speed = speed
+
+        def go_to(self, x, y):
+            '''
+            функция должна заставлять идти объект к точке (x,y)
+            желательно еще связать это со скоростью
+            функция будет использоваться восновном для MPC
+            '''
+
+        def go_button(self, w,a,s,d):
+            '''
+            функция перемещения для игрока
+            w,a,s,d -- булевские переменные соответствующие нажатию на такие же кнопки
+            true -- кнопка
+            не забыть про одновременное нажатие на кнопок и постоянную скорость
+            '''
+
+
+        class MPC():
+
+            class peaceful():
+
+            class enemy():
+                def attack(self):
+                    pass
+
+        class player():
+            def change_item(self):
+                '''
+                функция меняет предмет в руке
+                '''
+
+            def change_armor(self):
+                '''
+                функция меняет броню
+                '''
+
+
+
