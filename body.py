76cad89686b4de01d94db60e01b805df84c9f3c5
DronShock <strelnikov.av@phystech.edu>
2020-11-21 18:16:57 +0300

    Сделал ходьбу, загрузил карту и начал создавать анимации

--- body.py
+++ body.py
@@ -1,6 +1,8 @@
-import pygame as pg
+import pygame
+import random
+from os import path
 
-# from game_objects import *
+img_dir = path.join(path.dirname(__file__), 'img')
 
 # какой то текст
 # считывание размеров экрана
@@ -8,165 +10,183 @@ import pygame as pg
 # user32 = ctypes.windll.user32
 # screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
-screen_width = 1000
-screen_height = 650
-YELLOW = (255, 255, 0)
+WIDTH = 1000
+HEIGHT = 800
+FPS = 60
+
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-FPS = 40
-active_buttons = []  # Массив с активными кнопками клавиатуры
-characters = []
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 
-pg.init()
-screen = pg.display.set_mode((screen_width, screen_height))
-screen.fill(BLACK)
-pg.display.update()
-clock = pg.time.Clock()
-
-finished = False
-main_menu = True
-esc_menu = False
-playing_game = False
-open_inventory = False
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
 
-class player():
-    def __init__(self, x, y, speed, image):
-        self.width = 50
-        self.height = 50
-        self.x = 0
-        self.y = 0
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = player_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH // 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+        self.speedy = 0
+        self.frame = 0
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 500
+
+    def update(self):
+        """
+        Обновляет положение игрока на экране
+        :return: None
+        """
+        self.speedx = 0
+        self.speedy = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.animation("left")
+            self.speedx = -3
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 3
+            self.animation("right")
+        if keystate[pygame.K_UP]:
+            self.animation("up")
+            self.speedy = -3
+        if keystate[pygame.K_DOWN]:
+            self.animation("down")
+            self.speedy = 3
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+    def animation(self, move):
+        time = pygame.time.get_ticks()
+        if time - self.last_update < self.frame_rate:
+            self.last_update = time
+            if move == "up":
+                for anim in player_anim_up:
+                    self.frame += 1
+                    if self.frame == len(player_anim_up):
+                        anim in player_anim_up[0]
+                    else:
+                        self.image = pygame.image.load(path.join(img_dir, anim)).convert()
+                        self.image.set_colorkey(BLACK)
+
+    def change_item(self):
+        """
+        функция меняет предмет в руке
+        """
+
+    def change_armor(self):
+        """
+        функция меняет броню
+        """
+
+
+class Camera():
+    pass
+
+
+class obj():
+    pass
+
+
+class creature(obj):
+    def __init__(self, x, y, speed):
+        self.x = x
+        self.y = y
         self.speed = speed
-        self.image = pg.Surface((self.width, self.height))
-        self.image.fill((255, 255, 255))
 
     def go_to(self, x, y):
-        '''
+        """
         функция должна заставлять идти объект к точке (x,y)
         желательно еще связать это со скоростью
         функция будет использоваться восновном для MPC
-        '''
-        x += ((self.x - x) * self.speed) / (((y - self.y) ** 2 + (x - self.x) ** 2) ** 0.5 * FPS)
-        y += ((self.y - y) * self.speed) / (((y - self.y) ** 2 + (x - self.x) ** 2) ** 0.5 * FPS)
+        """
 
-    def go_button(self, button):
-        '''
+    def go_button(self, w, a, s, d):
+        """
         функция перемещения для игрока
         w,a,s,d -- булевские переменные соответствующие нажатию на такие же кнопки
         true -- кнопка
         не забыть про одновременное нажатие на кнопок и постоянную скорость
-        '''
-        if button == "w":
-            self.y += self.speed / FPS
-        if button == "a":
-            self.x -= self.speed / FPS
-        if button == "s":
-            self.y -= self.speed / FPS
-        if button == "d":
-            self.x += self.speed / FPS
-
-        if button == "wd":
-            self.y += self.speed * 0.71 / FPS
-            self.x += self.speed * 0.71 / FPS
-        if button == "wa":
-            self.y += self.speed * 0.71 / FPS
-            self.x -= self.speed * 0.71 / FPS
-        if button == "sa":
-            self.y -= self.speed * 0.71 / FPS
-            self.x -= self.speed * 0.71 / FPS
-        if button == "sd":
-            self.y -= self.speed * 0.71 / FPS
-            self.x += self.speed * 0.71 / FPS
+        """
 
 
-characters.append(player)
+class NPC(obj):
+    pass
 
 
-def check_keyboard_buttons(event):
-    """Функция записывает нажатые кенопки в массив active_buttons"""
-    if event.type == pg.KEYDOWN:
-        if event.key == pg.K_w:
-            active_buttons.append("w")
-        if event.key == pg.K_a:
-            active_buttons.append("a")
-        if event.key == pg.K_s:
-            active_buttons.append("s")
-        if event.key == pg.K_d:
-            active_buttons.append("d")
-
-    if event.type == pg.KEYUP:
-        if event.key == pg.K_w:
-            active_buttons.remove("w")
-        if event.key == pg.K_a:
-            active_buttons.remove("a")
-        if event.key == pg.K_s:
-            active_buttons.remove("s")
-        if event.key == pg.K_d:
-            active_buttons.remove("d")
+class peaceful(NPC):
+    pass
 
 
-def do_command():
-    """Выполняет команды исходя из нажатых кнопок"""
-    global esc_menu
-    global open_inventory
-
-    if "w" in active_buttons:
-        player.go_button("w")
-    if "a" in active_buttons:
-        player.go_button("a")
-    if "s" in active_buttons:
-        player.go_button("s")
-    if "d" in active_buttons:
-        player.go_button("d")
-    if "w" in active_buttons and "d" in active_buttons:
-        player.go_button("wd")
-    if "w" in active_buttons and "a" in active_buttons:
-        player.go_button("wa")
-    if "s" in active_buttons and "a" in active_buttons:
-        player.go_button("sa")
-    if "s" in active_buttons and "d" in active_buttons:
-        player.go_button("sd")
+class enemy(NPC):
+    def attack(self):
+        pass
 
 
-# def update_characters_image():
-#    for character in characters:
-#        character.image.blit(screen, (character.x,character.y))
-#        pg.display.update()
+# Загрузка изображений объектов
+background = pygame.image.load(path.join(img_dir, 'DB32RecolorEx.png')).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, "player.png")).convert()
 
-def button_start_game(x, y):
-    if (x >= 314) and (x <= 688) and (y >= 90) and (y <= 178):
-        return True
-    else:
-        return False
+# Создание массивов с анимациями
+player_anim_up = ['Up 0.png', 'Up 1.png', 'Up 0.png', 'Up 2.png']
+player_anim_down = [('Down 0.png'), ('Down 1.png'), ('Down 0.png'), ('Down 2.png')]
+player_anim_left = [('Left 0.png'), ('Left 1.png'), ('Left 0.png'), ('Left 2.png')]
+player_anim_right = [('Right 0.png'), ('Right 1.png'), ('Right 0.png'), ('Right 2.png')]
 
+# Добавления спрайтов в группу для отрисовки
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
-menu_pict = pg.image.load('main_menu1.png')
+main_menu = False
+esc_menu = False
+playing_game = True
+open_inventory = False
+finished = False
 
+# Цикл игры
 while not finished:
     clock.tick(FPS)
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # проверка для закрытия окна
             finished = True
-        # update_characters_image()
-        if main_menu == True:
-            # события в главном меню
-            screen.blit(menu_pict, (0, 0))
-            pg.display.update()
-            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-                click_x, click_y = event.pos
-                if button_start_game(click_x, click_y) == True:
-                    main_menu = False
-                    playing_game = True
-                    screen.fill(BLACK)
-                    pg.display.update()
-        if playing_game == True:
-            check_keyboard_buttons(event)
-            do_command()
+    if main_menu == True:
+        pass
+    # события в главном меню
+    if playing_game == True:
+        # события игры
+        # не забыть про open_inventory
+        # во время открытого инвенторя игра продолжается
 
-            # события игры
-            # не забыть про open_inventory
-            # во время открытого инвенторя игра продолжается
-        if esc_menu == True:
-            pass
-            # события меню паузы
+        # Обновление
+        all_sprites.update()
 
-pg.quit()
+        # Рендеринг
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+        all_sprites.draw(screen)
+        # Переворачиваем экран после отрисовки
+        pygame.display.flip()
+
+    if esc_menu == True:
+        pass
+    # события меню паузы
+pygame.quit()
