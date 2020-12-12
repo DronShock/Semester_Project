6e3100d25cac66ead3a76a22a37970ca746e522b
khomiakov <khomiakov.io@phystech.edu>
2020-12-12 16:01:21 +0300

    пофиксил баг с выходом из игры

--- main.py
+++ main.py
@@ -25,9 +25,8 @@ current_map = redactor_map(map1, player)
 (background, background_rect) = load_map(current_map.img_dir, img_dir)
 sozdanie_textov(g.screen)
 
-finished = False
 clock = pygame.time.Clock()
-while not finished:
+while not g.finished:
     g.step(active_sprites, background, background_rect, main_menu_pict, img_dir, current_map, map1, map2, map3,
            health_bar, mobs, player_anim_up, player_anim_down, player_anim_left, player_anim_right, player_udar_up,
            player_udar_down, player_udar_left, player_udar_right,player_sprite,objects,skelet_anim_up, skelet_anim_down, skelet_anim_right,
